c8383ca4c298237504a580552245791c92487fea
James Waite <James Waite>
2017-08-28 13:23:12 -0400

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,3 +15,47 @@ var config = {
 
   var initialLoad = true;
 
+  var newTableData = function(snapshot){
+  	var trainName = snapshot.val().trainName;
+  	var trainDestination = snapshot.val().trainDestination;
+  	var initialTime = snapshot.val().initialTime;
+  	var trainFrequency = snapshot.val().trainFrequency;
+
+  	var today = moment();
+  	var inputDate = moment(date, "DD/MM/YY");
+  	var r = $("<tr>");
+  	var dataValues = [trainName, trainDestination, initialTime, trainFrequency];
+  	var freq = "";
+
+  	for (var i = 0; i < 3; i++) {
+  		var data = $("<td>");
+  		data.html(dataValues[i]);
+  		r.append(data);
+  	}
+
+  	$("#table-body").append(r);
+
+  }
+
+  $("submit").on("click", function(event) {
+
+  	var trainName = $("#new-name").val().trim();
+  	var trainDestination = $("#new-destination").val().trim();
+  	var initialTime = $("#new-time").val().trim();
+  	var trainFrequency = $("#new-frequency").val().trim();
+
+  	database.ref().push({
+  		trainName: trainName,
+  		trainDestination: trainDestination,
+  		initialTime: initialTime,
+  		trainFrequency: trainFrequency,
+  		dateAdded: firevase.database.ServerValue.TIMESTAMP
+  	})
+
+  })
+
+  database.ref().on("child_added", function(snapshot) {
+  	console.log(snapshot.val());
+  	console.log("I'm making the table now!");
+  	newTableData(snapshot);
+  })
